b9dd76f0fc1950560dd4aa1e076ac0bbc0705bbf
guybracha <guy.bracha@gmail.com>
2023-07-07 20:03:00 +0300

    more info to the class

--- firstClass.py
+++ firstClass.py
@@ -1,13 +1,26 @@
 #define class
 class Dog:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+    
+    def get_age(self):
+        return self.age
+    
     def add_one(self, x):
-        return x + 1
+        return x + 2.5
 
     def bark(self):
         print("woof")
 
 # d variable
-d = Dog()
-d.bark()
-print(d.add_one(5))
-print(type(d))
+d1 = Dog("Guy", 25)
+d1.bark()
+print(d1.name)
+print(d1.age)
+print(d1.add_one(7.5))
+print(type(d1))
