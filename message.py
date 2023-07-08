6b56f906a4699997ca89e91877c5660e7a36f8a9
guybracha <guy.bracha@gmail.com>
2023-07-08 19:31:24 +0300

    new py file

---
+++ message.py
@@ -0,0 +1,9 @@
+name = input("enter your name: ")
+course = '''
+Dear {},
+Thank you for applying our new space program.
+Final result will arrive soon,
+Dean of Bracha University :)
+'''.format(name)
+
+print(course)
