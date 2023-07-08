833d7f2776f4f767ea557c94d91260a1be9799ac
guybracha <guy.bracha@gmail.com>
2023-07-08 09:27:56 +0300

    new projects

---
+++ inherit.py
@@ -0,0 +1,31 @@
+class Pet:
+    def __init__(self, name, age):
+       self.name = name
+       self.age = age
+
+    def show(self):
+       print(f"I am {self.name} and I am {self.age} years old")
+
+    def speak(self):
+        print("I don't know what i say")
+
+class Cat(Pet):
+    def speak(self):
+        print("Meow")
+
+class Dog(Pet):
+    def speak(self):
+        print("Bark")
+
+class Fish(Pet):
+    pass
+
+p = Pet("tim",19)
+p.show()
+c = Cat("bill",34)
+c.show()
+c.speak()
+d = Dog("jill",25)
+d.show()
+f = Fish("bubbles", 10)
+f.speak()
