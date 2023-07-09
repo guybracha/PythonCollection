a5d2cd3b2444cdd131a5fc37bb9b16d2642849f7
guybracha <guy.bracha@gmail.com>
2023-07-09 09:33:14 +0300

    location

--- message.py
+++ message.py
@@ -3,7 +3,7 @@ course = '''
 Dear {},
 Thank you for applying our new space program.
 Final result will arrive soon,
-Dean of Bracha University :)
+Dean of Bracha University of Herzliya, Israel :)
 '''.format(name)
 
 print(course)
